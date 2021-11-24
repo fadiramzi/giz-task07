d844900042874bbff93bab6cc08d0d237740d485
Mahmood83367 <84744874+Mahmood83367@users.noreply.github.com>
2021-11-24 22:17:37 +0300

    add not completed search bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import Header from './components/Header';
 import './components/styles.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Products from './components/Products';
 import Content from './components/Content';
 
 function App() {
-  const [products,setProducts] = useState(Content)
+  const [products] = useState(Content)
+  const [itemFound,setItemFound] = useState(products)
+  const [itemFound1,setItemFound1] = useState([])
   
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
+
     const deleteProduct = (id) => {
-      setProducts(products.filter((product) => product.id !== id ))
+      setItemFound(itemFound.filter((product) => product.id !== id ))
     }
     
     const addProduct = (product) => {
       const id = Math.floor(Math.random()*10000)+1
       const newProduct = {id, ...product}
-      setProducts([...products,newProduct])
+      setItemFound([...itemFound,newProduct])
+  }
+
+  const searchHandler = (e) => {
+    setItemFound1(itemFound)
+    console.log(products)
+    setItemFound1(itemFound.filter((item) => {
+         return  item.productName.toLowerCase().includes(e.target.value.toLowerCase())
+       }))
+       setItemFound(itemFound1)
   }
 
   return (
     <div>
-      <Header onAdd={addProduct} />
-      {products.length>0 ? <Products products={products} onDelete={deleteProduct}  /> : 'No products to show' }
+      <Header onAdd={addProduct} onChange={searchHandler} />
+      {itemFound.length>0 ? <Products products={itemFound} onDelete={deleteProduct}  /> : 'No products to show' }
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react'
 import AddItem from './AddItem'
 import Input from './Input'
 
-const Header = ({onAdd}) => {
+const Header = ({onAdd, onChange}) => {
     return (
         <header className='headerCls'>
             <div className='headerDiv'>
               <div>
-                <Input disc='Search for products here...' type='text' styles='searchTxt' />
+                <Input disc='Search for products here...' type='text' styles='searchTxt' onChange={onChange} />
               </div>
               <div>
                 <AddItem onAdd={onAdd} />
