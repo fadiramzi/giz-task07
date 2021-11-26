d1f075012b9f7afdff739b8c390bddb46c8220b4
Saba Salman <sabamsalman@gmail.com>
2021-11-26 10:37:21 +0300

    add new btn functionality is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,65 @@
-// import { useState } from 'react'
+import React,{useState} from 'react'
 import './App.css'
 import Card from './Card'
 import Button from './Button'
 import Input from './Input'
 
-// <Card name='Air Max 95 U' price='1.99' desc='Sneakers'/>
-
 function App() {
-    let products = [
+    const [rerender, setRerender] = useState(false)
+    const [products, setProducts] = useState([
         {
-            name: "Air Max 95 U",
+            name: "A",
             price: 99,
             desc: "Sneakers"
         },
         {
-            name: "Air Max 95 U",
+            name: "B",
             price: 99,
             desc: "Sneakers"
         },
         {
-            name: "Air Max 95 U",
+            name: "C",
             price: 99,
             desc: "Sneakers"
         },
-    ]
-    let cards = products.map((p, i) => <Card name={p.name} price={p.price} desc={p.desc} key={i}/>)
+    ])
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState(0)
+    const [search, setSearch] = useState("", () => {
+        console.log("asdasdasd")
+    })
 
-    let add_prod_name = ""
-    let add_prod_price = 0
-
+    function on_prod_name_changed(e) { setName(e.target.value) }
+    function on_prod_price_changed(e) { setPrice(e.target.value) }
     function add_product(e) {
-        // alert(a)
-        // products.push({
-        // })
+        let new_products = products
+        new_products.push({
+            name: name,
+            price: price,
+            desc: "Sneakers"
+        })
+        setProducts(new_products)
+        setRerender(!rerender)
     }
-
-    add_product()
+    function on_search_changed(e) {
+        setSearch(e.target.value)
+        let search_term = search
+        let new_products = products.filter(p => p.name == search_term)
+        // console.log(new_products)
+        // setProducts(new_products)
+        // setRerender(!rerender)
+    }
 
     return (
         <div className="App">
-
             <div className="controls">
-                <div className="search_box"><Input placeholder="Search Products"/></div>
+                <div className="search_box"><Input placeholder="Search Products" onChange={on_search_changed}/></div>
                 <div></div>
-                <Input placeholder="Name"/>
-                <Input placeholder="Price"/>
+                <Input placeholder="Name" onChange={on_prod_name_changed}/>
+                <Input placeholder="Price" onChange={on_prod_price_changed}/>
                 <Button value="Add New" onClick={add_product}/>
             </div>
-
-            <div className="grid">{cards}</div>
+            <div className="grid">{products.map((p, i) => <Card name={p.name} price={p.price} desc={p.desc} key={i}/>)}</div>
         </div>
     )
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -2,7 +2,7 @@ import './Input.css'
 
 function Input(props) {
     return (
-        <input className="Input" type="text" placeholder={props.placeholder} value={props.value}/>
+        <input className="Input" type="text" placeholder={props.placeholder} value={props.value} onChange={props.onChange}/>
     )
 }
 
