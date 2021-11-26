889f44fbf53feb5a072f4ca36531b038d245932e
abdullahamer <abbnntt5565@gmail.com>
2021-11-26 12:52:12 +0300

    aa

---
+++ hello-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Page/Home/Home'
+// import Home from './components/WelcomeClass'
+function App() {
+
+    return (
+        <div >
+            <Home/>
+        </div>
+    );
+}
+
+export default App;
