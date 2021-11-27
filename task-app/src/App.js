3c18affd04338efecc26df33b800ab348e364a46
mahdeesharef <mahdeesharef@gmail.com>
2021-11-27 14:43:32 +0300

    add task

---
+++ task-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ProductList } from './components/ProductList';
+
+function App() {
+  return (
+    <div className="container">
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ task-app/src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export function ProductCard(props) {
+  return (
+    <div className="col-md-3 col-xs-6">
+      <div className="card">
+        <img className="card-img-top" src="http://via.placeholder.com/350x250" alt=""></img>
+        <div className="card-body">
+          <p >
+     
+          </p>
+          <p>
+        
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
