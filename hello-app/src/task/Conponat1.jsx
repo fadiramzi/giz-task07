34cea9888d8a9e48e352af33eb88290e57de36ba
ROQAYA_AMEER <roqayaameer312@gmail.com>
2021-12-16 15:40:44 +0300

    task7

--- hello-app/src/task/Conponat1.jsx
+++ hello-app/src/task/Conponat1.jsx
@@ -1,20 +1,26 @@
-import React from "react";
-import styles from './fildes.module.css'
+import React, { Fragment } from "react";
+import styles from './componat1.module.css'
 import pair from './pair.jpg';
 import { useState } from "react";
 
-const  Conponat1= ({nameslist}) => {
-    const cards = nameslist.map(({name,price},idx) => (
-        <div className={styles.maincontenar} key={idx}>
-            <div><img src={pair} alt="pair"
-              width={"80px"}
-              height={"80px"} /></div>
-            <div>name:{name}</div>
-            <div>price:{price}</div>
-          </div>
-      ))
-      return(
-          <div>{cards}</div>
-      )
+const Conponat1 = ({ nameslist, deleteHandler }) => {
+  const cards = nameslist.map(({ name, price }, idx) => (
+    <Fragment >
+      <div className={styles.pro} key={idx}>
+        <div className={styles.delete}>
+          <button onClick={(e) => deleteHandler(e, idx)} >X</button>
+        </div>
+        <img src={pair} alt="pair"
+          width={"80px"}
+          height={"80px"} />
+        {name}
+        {price}
+        <button className={styles.add_prodect} >add prodect</button>
+      </div>
+    </Fragment>
+  ))
+  return (
+    <div>{cards}</div>
+  )
 }
 export default Conponat1;
--- hello-app/src/task/Fildes.jsx
+++ hello-app/src/task/Fildes.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styles from './fildes.module.css'
 import pair from './pair.jpg';
 import Conponat1 from './Conponat1'
 import Search from './Search';
 import { useState } from "react";
 const Fildes = () => {
-  const [filter,setfilter]=useState("")
+  const [filter, setfilter] = useState("")
+  const [name,setname]= useState('')
+  const [price,setprice]= useState('')
   const [state, setState] = useState([
     {
       name: "shoes",
@@ -13,7 +15,7 @@ const Fildes = () => {
 
     },
     {
-      name: "hhhhh",
+      name: "kkk",
       price: "10$"
 
     },
@@ -23,26 +25,54 @@ const Fildes = () => {
 
     }
   ]);
-  const searchPrice = (name) =>
-  {
-   setfilter(name);
-  }
-  const priceHandeler =() =>{ 
-   if (filter.length !== 0){
-      return state.filter((el) => el.name.includes(filter) )
+  const [list, setList] = React.useState(state);
+  const handleSubmit = event => {
+    if (name && price) {
+      setList(list.concat(name,price));
     }
-    return state;}
+    setname('');
+    setprice('');
+    event.preventDefault();
+    console.log(name,price)
+  };
 
+  const searchPrice = (name) => {
+    setfilter(name);
+  }
+  const priceHandeler = () => {
+    if (filter.length !== 0) {
+      return state.filter((el) => el.name.includes(filter))
+    }
+    return state;
+  }
+  
+  const deleteHandler = (e,clickeidx)=>{
+     const deleteProdect = state.filter((el,idx) => idx !== clickeidx);
+     setState(deleteProdect)
+  }
   return (
-    <div className={styles.cardmap}>
-      <input type="text" placeholder="name" />
-      <input type="text" placeholder="price"/>
-      <button>add</button>
-      <div>
-        <Search  searchPrice={searchPrice}/>
+    <Fragment >
+      <div className={styles.cardmap}>
+        <div>
+          <Search searchPrice={searchPrice} />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+          placeholder="name"  
+          value={name} 
+          onChange={(e)=> setname(e.target.value)}/>
+
+          <input type="text" 
+          placeholder="price" 
+          value={price} 
+          onChange={(e)=> setprice(e.target.value)}/>
+          <button >add</button>
+        </form>
       </div>
-      <Conponat1 nameslist={priceHandeler()} />
-    </div>
+      <div>
+        <Conponat1 nameslist={priceHandeler()} deleteHandler={deleteHandler}/>
+      </div>
+    </Fragment>
   )
 }
 export default Fildes;
